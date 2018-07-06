1ad727cce48fa21e183bf29a9d78acbefaba22a0
vedro-compota <ru-ibn-hottab@yandex.ru>
2018-07-06 17:45:35 +0300

    Update bashrc --new commands.sh
    Добавлена функция синхронизации удалённых репозиториев gsync

--- bashrc --new commands.sh
+++ bashrc --new commands.sh
@@ -23,3 +23,43 @@ function gacp() {
     git commit -m "$1"
     git push
 }
+
+# Sync branch of all known for this local repo remomte repos
+# Call example (for master branch):
+#  $  gsync master
+function gsync() {
+    RemoteReposNames=() # объявляем массив
+    RemoteDataText="$(git remote -v)"
+    #echo "${RemoteDataText}"
+
+    while read -r line; do
+        #echo "... $line ..."
+        words=($line)
+         #printf "%s\n" "${words[@]}"
+         #echo '--'
+        RemoteReposNames+=("${words[0]}")  # добавляем элемент в массив
+         #printf "%s\n" "${RemoteReposNames[@]}"
+         #echo '------'
+    done <<< "$RemoteDataText"
+
+    # оставляем только уникальные значения
+    RemoteReposNames=($(echo "${RemoteReposNames[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+
+    echo 'Remotes in your repo are:'
+    printf "%s\n" "${RemoteReposNames[@]}"
+
+    echo 'Starting sync...'
+    for remoteName in "${RemoteReposNames[@]}"
+    do
+      echo "Pull $remoteName $1----------- " 
+      git pull $remoteName $1
+    done
+
+    for remoteName in "${RemoteReposNames[@]}"
+    do
+      echo "Push $remoteName $1---------- " 
+      git push $remoteName $1
+    done
+
+    echo 'Sync is finished.'
+}
